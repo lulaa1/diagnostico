3f8bda2beb45d3312ab9811a84727e29e47d2720
lulaa1 <133403327+lulaa1@users.noreply.github.com>
2024-03-04 07:42:22 -0800

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,23 @@
+def obtener_nombre_dia (numero)
+nombre_dia= ""
+if numero= 1:
+nombre_dia="Lunes"
+if numero= 2:
+nombre_dia= "Martes"
+if numero= 3:
+nombre_dia= "Miercoles"
+if numero= 4:
+nombre_dia= " Jueves"
+if numero= 5:
+nombre_dia= "Viernes"
+if numero= 6:
+nombre_dia "sabado"
+if numero= 7:
+nombre_dia "Domingo"
+elif:
+nombre_dia=" Numero invalido"
+return nombre_dia
+numero_dia= int(input("ingrese un numero del 1 a 7 para representar un dia de la semana:))
+nombre_dia=
+obtener_nombre_dia (numero_dia)
+print(" El dia correspondiente al numero ingresado es:"nombre_dia)
